c7202d6ae65b18eca00c448a1c03c5c2a8e8ad0b
Stefan Guggisberg <guggs2@bfh.ch>
2022-06-26 09:37:48 +0200

    clean up

--- backend/src/main/java/ch/fit4bit/controller/TrainingTypController.java
+++ backend/src/main/java/ch/fit4bit/controller/TrainingTypController.java
@@ -8,11 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ch.fit4bit.dto.TrainingTypDTO;
 import ch.fit4bit.entity.TrainingTyp;
@@ -30,6 +26,11 @@ public class TrainingTypController {
 	public TrainingTypController(TrainingTypService trainingTypService, ModelMapper modelMapper) {
 		this.trainingTypService = trainingTypService;
 		this.modelMapper = modelMapper;
+	}
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody TrainingTypDTO trainingTypDTO) {
+		trainingTypService.create(modelMapper.map(trainingTypDTO, TrainingTyp.class));
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 
 	@GetMapping
